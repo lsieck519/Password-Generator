095ee9ab9406644956fa7f3722e1d916d392aac9
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2022-12-10 13:14:27 -0600

    changed css formatting and added more notes to js

--- Password-Generator/Develop/script.js
+++ Password-Generator/Develop/script.js
@@ -1,7 +1,7 @@
 
 // Assignment code here
 
-//Global Arrays
+//Global variables 
 
 var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -9,76 +9,72 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['~','!','@','#','$','%','^','&','*','?','=','+','<','>','/','[',']','{','}']
 
 
-//Function to custom generate
+//Function to custom generate a password
 
 function generatePassword() {
+
+//Local variable for this function
  
 var possibleCharacters = [];
 
-// Prompts and alerts to obtain and confirm preferences 
+// Prompts and alerts to obtain and confirm preferences for custom generated password
+
+//Including a default value of 16 to reduce likelihood of user entering an invalid number 
 
 passwordLength = prompt("How many characters do you want in your password? Choose between 8-128 characters.","16");
 if (passwordLength < 8 || passwordLength > 128) {
-  return "Please enter a whole number between 8 and 128.";
+  return "Please try again and enter a whole number between 8 and 128.";
 } else if (isNaN(passwordLength)) {
-  passwordLength = prompt("Please enter a valid whole number between 8 and 128");
-}
-else {
+  passwordLength = prompt("Please try again and enter a valid whole number between 8 and 128");
+} else {
   alert("Your password will be " + passwordLength + " characters long.");
 }
 
 hasLowercase = confirm("Do you want lowercase characters? Select OK for YES or select Cancel for NO");
 if (hasLowercase) {
   alert("Your secure password will include at least one lowercase character.");
-}
-else {
+} else {
   alert("Your password will NOT have any lowercase characters.");
 }
 
 hasUppercase = confirm("Do you want uppercase characters? Select OK for YES or select Cancel for NO");
 if (hasUppercase) {
   alert("Your secure password will include at least one uppercase character.");
-}
-else {
+} else {
   alert("Your password will NOT have any uppercase characters.");
 }
 
 hasNumbers = confirm("Do you want to use numbers? Select OK for YES or select Cancel for NO");
 if (hasNumbers) {
   alert("Your secure password will include at least one number");
-}
-else {
+} else {
   alert("Your password will NOT have any numbers.");
 }
 
 hasSpecial = confirm("Do you want special characters Select OK for YES or select Cancel for NO");
 if (hasSpecial) {
   alert("Your password will include at least one special character.");
-}
-else {
+} else {
   alert("Your password will NOT have any special characters.");
 }
 
 if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
-  return "Unable to generate password without characters. Please select at least one character type.";
+  return "Unable to generate password without characters. Please try again and select at least one character type.";
 };
 
 // Combine selected characters type into one variable called possibleCharacters
 
 if (hasLowercase) {
   possibleCharacters = possibleCharacters.concat(lowercaseLetters);
-}
-if (hasUppercase) {
+} if (hasUppercase) {
   possibleCharacters = possibleCharacters.concat(uppercaseLetters);
-}
-if (hasNumbers) {
+} if (hasNumbers) {
   possibleCharacters = possibleCharacters.concat(numbers);
-}
-if (hasSpecial) {
+} if (hasSpecial) {
   possibleCharacters = possibleCharacters.concat(specialCharacters);
 }
 
-// Use math objects to randomize selected characters into a variable called securePassword at the correct length 
+// Use math objects to randomize selected possible characters into a variable called securePassword at the correct length 
 
 let securePassword = ""
 for (let i = 0; i < passwordLength; i++) {
@@ -90,13 +86,14 @@ return securePassword;
 };
 
 
-// Function to auto generate secure password without customization (includes all character types)
+// Function to fast-track auto generate secure password without customization. Includes all character types and a strong password length. 
 
 function generatePassword2() {
 
 var secureCharacters = [];
 secureCharacters = secureCharacters.concat(numbers, lowercaseLetters, uppercaseLetters, specialCharacters);
 secureLength = 81
+
 
 let securePassword2 = ""
 for (let i = 0; i < secureLength; i++) {
@@ -122,7 +119,6 @@ function writePassword() {
 }
 
 // Write auto password to #password input 
-
 function writePassword2() {
   var password = generatePassword2();
   var passwordText = document.querySelector("#password");
